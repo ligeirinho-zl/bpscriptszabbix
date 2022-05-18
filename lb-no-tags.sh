438407a431fc3b5765463ac2b47449309dae852a
ligeirinho-zl <44400925+ligeirinho-zl@users.noreply.github.com>
2022-05-18 17:54:03 -0300

    Delete lb-no-tags.sh

--- lb-no-tags.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/bash
-
-
-aws elbv2 describe-load-balancers --query "LoadBalancers[*].DNSName" --output text
-aws elb describe-load-balancers --query "LoadBalancerDescriptions[*].DNSName" --output text
-
